de16067bbe77a681707a5ad4a0733aed663c06a0
Nitesh Jangir <jnitesh054@gmail.com>
2026-04-26 21:11:53 +0530

    fix routing

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,17 +1,46 @@
 const jwt = require("jsonwebtoken");
 
 module.exports.verifyToken = (req, res, next) => {
-  const token = req.cookies.accessToken;
+  let accessToken = req.cookies.accessToken;
 
-  if (!token) {
-    return res.status(401).json({ message: "Unauthorized: No access token" });
+  // 1. If access token exists, verify it
+  if (accessToken) {
+    try {
+      const decoded = jwt.verify(accessToken, process.env.ACCESS_SECRET);
+      req.userId = decoded.id;
+      return next();
+    } catch (err) {
+
+
+    }
+  }
+
+
+  const refreshToken = req.cookies.refreshToken;
+
+  if (!refreshToken) {
+    return res.status(401).json({ message: "Unauthorized: no token" });
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.ACCESS_SECRET);
+    const decoded = jwt.verify(refreshToken, process.env.REFRESH_SECRET);
+
+    const newAccessToken = jwt.sign(
+      { id: decoded.id },
+      process.env.ACCESS_SECRET,
+      { expiresIn: "15m" }
+    );
+
+    res.cookie("accessToken", newAccessToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      maxAge: 15 * 60 * 1000,
+    });
+
     req.userId = decoded.id;
     next();
-  } catch (e) {
-    return res.status(401).json({ message: "Invalid access token" });
+  } catch (err) {
+    return res.status(401).json({ message: "Unauthorized: invalid token" });
   }
 };
