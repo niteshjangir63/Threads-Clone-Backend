2c060af18f8345914589bf53f2512e959686377a
Nitesh Jangir <jnitesh054@gmail.com>
2026-05-05 19:52:52 +0530

    added post update route

--- routes/Post.js
+++ routes/Post.js
@@ -9,7 +9,7 @@ router.get("/posts", getPosts);
 router.get("/post/:id",getPostById)
 router.delete("/post/delete/:id",verifyToken,deletePost);
 router.post("/post/like/:id",verifyToken,likePost)
-router.post("/edit/post",verifyToken,editPost)
+router.put("/edit/post",verifyToken,editPost)
 
 
 module.exports = router;
